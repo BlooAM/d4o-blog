c09e6e37d5fcd55b148827375f7d72fedc2f3329
BlooAM <adam3200@vp.pl>
2024-06-25 10:15:36 +0200

    adjust boot time config

--- scripts/dev/setup-db.sh
+++ scripts/dev/setup-db.sh
@@ -1,3 +1,2 @@
-source scripts/dev/.settings
-docker pull postgres:16.0
-docker run --name postgres-dev --env PGUSER=$POSTGRES_USER --env POSTGRES_USER=$POSTGRES_USER --env POSTGRES_PASSWORD=$POSTGRES_DEV_PASSWORD -d postgres:16.0
+source ./.env
+docker-compose -f ./scripts/dev/docker-compose.yml up -d
