a62edd473c16cdb106ae13bffa2881b3d0801afb
BlooAM <adam3200@vp.pl>
2024-08-31 09:36:20 +0200

    update local db setup script

--- scripts/dev/setup-db.sh
+++ scripts/dev/setup-db.sh
@@ -1,2 +1,4 @@
-source ./.env
-docker-compose -f ./scripts/dev/docker-compose.yml up -d
+source scripts/dev/.env
+echo $POSTGRES_DEV_PASSWORD
+docker build -t postgres-dev -f scripts/dev/Dockerfile_dev --build-arg POSTGRES_DEV_USER=root --build-arg  POSTGRES_DEV_PASSWORD=$POSTGRES_DEV_PASSWORD --build-arg POSTGRES_DEV_PORT=$POSTGRES_DEV_PORT ./
+docker run -d -it --name postgres-dev-img -p 5432:5432 postgres-dev
