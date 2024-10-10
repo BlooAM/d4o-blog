542d5468c4fb985ca41519981674e1a8a57fb46d
BlooAM <adam3200@vp.pl>
2024-10-10 10:12:26 +0200

    more name fixing

--- d4o_blog/blog/views.py
+++ d4o_blog/blog/views.py
@@ -91,7 +91,7 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            result = Post.published.annotate(
+            results = Post.published.annotate(
                 search=SearchVector('title', 'body')
             ).filter(search=query)
 
