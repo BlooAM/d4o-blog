672bb1651af4607643ed5f64d5d0d8f0a279f833
BlooAM <adam3200@vp.pl>
2024-10-30 18:40:57 +0100

    some type hints

--- data/main.py
+++ data/main.py
@@ -1,4 +1,6 @@
+import random
 import os
+import typing as t
 
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
@@ -6,8 +8,17 @@ from newsapi import NewsApiClient
 
 load_dotenv()
 client = NewsApiClient(api_key=os.getenv('NEWSAPI_KEY'))
+SOURCES_LIMIT: int = 10
+TApiReponse = dict[str, str | list[str]]
+
+
+def _parse_response(response: TApiReponse):
+    pass
+
 
 sources_response = client.get_sources()
-sources = sources_response.get('sources')
+sources: list[TApiReponse] = sources_response.get('sources')
 if sources:
-    article_responses = [client.get_everything(sources=source['id']) for source in sources[:10]]
+    article_responses: list[TApiReponse] = [
+        client.get_everything(sources=source['id']) for source in random.suffle(sources)[:SOURCES_LIMIT]
+    ]
