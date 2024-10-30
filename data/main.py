4084b091103fb9779d24425a54132212c7783b9f
BlooAM <adam3200@vp.pl>
2024-10-30 19:32:37 +0100

    fix CURR_DIR path

--- data/main.py
+++ data/main.py
@@ -10,8 +10,8 @@ from pathlib import Path
 
 load_dotenv()
 client = NewsApiClient(api_key=os.getenv('NEWSAPI_KEY'))
-CURR_DIR = Path(__file__)
-SOURCES_LIMIT: int = 10
+CURR_DIR = Path(__file__).parent
+SOURCES_LIMIT: int = 2
 TApiReponse = dict[str, str | list[str]]
 
 
