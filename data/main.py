363433fb7a159155e8bc02452a3b190f5fb19950
BlooAM <adam3200@vp.pl>
2024-10-30 19:28:34 +0100

    test sample data statics

--- data/main.py
+++ data/main.py
@@ -1,12 +1,16 @@
+import json
 import os
 
 from dotenv import load_dotenv
 import inflection
 from newsapi import NewsApiClient
+from newsapi.newsapi_exception import NewsAPIException
+from pathlib import Path
 
 
 load_dotenv()
 client = NewsApiClient(api_key=os.getenv('NEWSAPI_KEY'))
+CURR_DIR = Path(__file__)
 SOURCES_LIMIT: int = 10
 TApiReponse = dict[str, str | list[str]]
 
@@ -24,8 +28,16 @@ def _parse_response(response: TApiReponse) -> list[TApiReponse] | None:
             return articles
 
 
-sources_response = client.get_sources()
-sources: list[TApiReponse] = sources_response.get('sources')
+try:
+    sources_response = client.get_sources()
+    sources: list[TApiReponse] = sources_response.get('sources')
+except NewsAPIException as e:
+    sample_data_dir = CURR_DIR / 'tests' / 'static'
+    sample_data_path = sample_data_dir / 'sample_sources.json'
+    with open(sample_data_path, 'r') as fp:
+        sources = json.load(fp)
+
+
 if sources:
     article_responses: list[TApiReponse] = [
         client.get_everything(sources=source['id']) for source in sources[:SOURCES_LIMIT]
