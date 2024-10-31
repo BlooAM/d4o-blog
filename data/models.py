c53d601d93d1a0281ef15acd5215a2346c0e1e9c
BlooAM <adam3200@vp.pl>
2024-10-31 17:44:00 +0100

    article pydantic model

--- data/models.py
+++ data/models.py
@@ -1,2 +1,13 @@
+import datetime as dt
+
 from pydantic import BaseModel
 
+
+class Article(BaseModel):
+    source: str
+    author: str
+    title: str
+    description: str
+    url: str
+    url_to_image: str
+    published_at: dt.datetime
